48f42f5e192aab28eac553e610d7e66089469e13
Sebastian009w <ia3266595@gmail.com>
2023-05-02 22:49:49 -0500

    folder api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage } from "./pages/Home";
 import { CreatePage } from "./pages/Create";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/Tasks" />} />
       <Route path="/Tasks" element={<HomePage />} />
       <Route path="/Create" element={<CreatePage />} />
     </Routes>
