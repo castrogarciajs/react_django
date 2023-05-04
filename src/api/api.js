6597592e4d8905fe0c39e723d3d9316c137e39c0
Sebastian009w <ia3266595@gmail.com>
2023-05-03 20:58:06 -0500

    datos de django

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const response = await axios.get("http://localhost:8000/tasks/api/v1/tasks/");
+export const get = () => axios.get("http://localhost:8000/tasks/api/v1/tasks/");
 
-console.log(response);
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { get } from "../api/api";
 import { Tasks } from "../components/Tasks";
 
 export function HomePage() {
+  useEffect(() => {
+    async function start() {
+      const response = await get();
+      console.log(response);
+    }
+    start();
+  });
   return <Tasks />;
 }
